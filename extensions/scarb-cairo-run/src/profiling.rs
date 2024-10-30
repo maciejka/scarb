fcee1785d25745530a42026e8dd87c4377b29dbd
Michael Zaikin <michael.zaikin@starkware.co>
2024-10-30 11:07:00 +0000

    Collapse recursive calls

--- extensions/scarb-cairo-run/src/profiling.rs
+++ extensions/scarb-cairo-run/src/profiling.rs
@@ -3,9 +3,10 @@ use std::{collections::HashMap, fs};
 use cairo_lang_runner::profiling::ProfilingInfo;
 use cairo_lang_sierra::program::Program;
 use camino::Utf8PathBuf;
+use itertools::Itertools;
 
 /// Saves post-processed stack trace with weights
-pub fn save_profiler_output(program: &Program, profiling_info: &ProfilingInfo, output_path: &Utf8PathBuf) -> anyhow::Result<()> {
+pub fn save_profiler_output_old(program: &Program, profiling_info: &ProfilingInfo, output_path: &Utf8PathBuf) -> anyhow::Result<()> {
     let format_stack_item = |(idx_stack_trace, weight): (&Vec<usize>, &usize)| {
         let key = index_stack_trace_to_name_stack_trace(program, &idx_stack_trace).join(";");
         format!("{key} {weight}")
@@ -22,22 +23,37 @@ pub fn save_profiler_output(program: &Program, profiling_info: &ProfilingInfo, o
     Ok(())
 }
 
-fn process(program: &Program, profiling_info: &ProfilingInfo) -> Vec<(Vec<String>, usize)> {
-
+/// Saves aggregated stack trace with weights
+pub fn save_profiler_output(program: &Program, profiling_info: &ProfilingInfo, output_path: &Utf8PathBuf) -> anyhow::Result<()> {
+    let mut stack_trace_agg: HashMap<String, usize> = Default::default();
     for (stack_trace, weight) in profiling_info.stack_trace_weights.iter() {
-        let mut rec_depth: HashMap<String, usize> = Default::default();
         let mut key_parts: Vec<String> = Vec::with_capacity(stack_trace.len());
-        for func_idx in stack_trace {
-            let func_name = program.funcs[*func_idx].id.to_string();
-            if let Some(depth) = rec_depth.get_mut(&func_name) {
-                *depth += 1;
-            } else {
-                key_parts.push(func_name.clone());
-                rec_depth.insert(func_name, 1);
+        for idx in stack_trace {
+            let func_name = program.funcs[*idx].id.to_string();
+            if let Some(last_name) = key_parts.last() {
+                if last_name == &func_name {
+                    continue;
+                }
             }
+            key_parts.push(func_name);
+        }
+
+        let key = key_parts.join(";");
+        if let Some(elt) = stack_trace_agg.get_mut(&key) {
+            *elt += weight;
+        } else {
+            stack_trace_agg.insert(key, *weight);
         }
     }
-    vec![]
+
+    let contents = stack_trace_agg
+        .into_iter()
+        .sorted()
+        .map(|(key, weight)| format!("{key} {weight}"))
+        .join("\n");
+
+    fs::write(output_path, contents)?;
+    Ok(())
 }
 
 /// Converts a stack trace represented as a vector of indices of functions in the sierra program to
